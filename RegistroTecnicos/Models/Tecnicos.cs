e5a8b6c7acf8b205809b5c934423eb0670e65324
Anderson <@1001ander>
2025-05-15 10:23:46 -0400

    Crear la carpeta Models y el modelo Tecnicos.

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO.Compression;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicosId { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "solo se permiten letras ")]
+    [Required(ErrorMessage = "Campo requerido")]
+    public string? Nombres { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El valor debe ser mayor que cero")]
+    [Required(ErrorMessage = "Campo requerido")]    
+    public decimal SueldoHora { get; set; }
+}
