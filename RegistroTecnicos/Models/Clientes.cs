2080e9aa844814dc37efa8988f21a23f16b23688
Anderson <@1001ander>
2025-05-23 22:46:59 -0400

    Corregir Tecnico en el Modelo para solucionar conflicto y agregar el date time en UtcNow.

--- RegistroTecnicos/Models/Clientes.cs
+++ RegistroTecnicos/Models/Clientes.cs
@@ -1,45 +1,45 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RegistroTecnicos.Models
+namespace RegistroTecnicos.Models;
+
+public class Clientes
 {
-    public class Clientes
-    {
-        [Key]
-        public int ClienteId { get; set; }
+    [Key]
+    public int ClienteId { get; set; }
 
-        [Required(ErrorMessage = "Campo Obligatorio")]
-        public DateTime? FechaIngreso { get; set; } = DateTime.Now;
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [Column(TypeName = "timestamp without time zone")]
+    public DateTime? FechaIngreso { get; set; } = DateTime.UtcNow;
 
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten Letras")]
-        [Required(ErrorMessage = "Campo Obligatorio")]
-        public string? Nombres { get; set; }
-       
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten Letras")]
-        [Required(ErrorMessage = "Campo Obligatorio")]
-        public string? Direccion { get; set; }
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten Letras")]
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? Nombres { get; set; }
+   
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "solo se permiten Letras")]
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? Direccion { get; set; }
 
-        [StringLength(11, ErrorMessage = "El Rnc no debe tener mas de 11 numeros")]
-        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
-        [Required(ErrorMessage = "Campo Obligatorio")]
+    [StringLength(11, ErrorMessage = "El Rnc no debe tener mas de 11 numeros")]
+    [RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
+    [Required(ErrorMessage = "Campo Obligatorio")]
 
-        public string? Rnc { get; set; }
+    public string? Rnc { get; set; }
 
-        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
-        [Required(ErrorMessage = "Campo Obligatorio")]
-        public decimal? LimiteCredito { get; set; }
+    [RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public decimal? LimiteCredito { get; set; }
 
-        //se inicia aqui con la relacion entre tablas 
-        [Required(ErrorMessage = "Campo obligatorio")]
-        [ForeignKey("TecnicoId")]
-        public int TecnicoId { get; set; }
+    //se inicia aqui con la relacion entre tablas 
+    [Required(ErrorMessage = "Campo obligatorio")]
+    [ForeignKey("TecnicoId")]
+    public int TecnicoId { get; set; }
 
-        ///aqui ponemos la instancia 
-        public Tecnicos? Tecnicos { get; set; }
-
+    ///aqui ponemos la instancia 
+    public Tecnicos? Tecnico { get; set; }
 
 
 
 
-    }
+
 }
