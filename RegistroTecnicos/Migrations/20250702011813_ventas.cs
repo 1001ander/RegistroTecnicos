80261676c178196b592c25cad1a43624e9802a95
Anderson <@1001ander>
2025-07-01 21:34:38 -0400

    crear la migracion de ventas.

---
+++ RegistroTecnicos/Migrations/20250702011813_ventas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class ventas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
