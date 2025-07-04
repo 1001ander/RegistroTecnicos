cfe2b65581551af50133b0fa7231704371e28b6c
Anderson <@1001ander>
2025-07-03 20:38:36 -0400

    Agregar el modelo paginacion.

---
+++ RegistroTecnicos/Models/Paginacion.cs
@@ -0,0 +1,24 @@
+﻿namespace RegistroTecnicos.Models;
+
+public class Paginacion<T>
+{
+    public List<T> Items { get; set; } = new();
+    public int TotalCount { get; set; }
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+
+    public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
+
+    public Paginacion() { }
+
+    public Paginacion(List<T> items, int totalCount, int pageIndex, int pageSize)
+    {
+        Items = items;
+        TotalCount = totalCount;
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+    }
+}
